62e496d3097979349722682fb8af837813e59fd6
Pedro Valladares <pedrohvalladares@hotmail.com>
2025-10-29 15:23:44 -0300

    Custom validator de CPF

---
+++ src/main/java/br/edu/infnet/pedrovalladaresapi/validation/validators/CpfValidator.java
@@ -0,0 +1,16 @@
+package br.edu.infnet.pedrovalladaresapi.validation.validators;
+
+import br.edu.infnet.pedrovalladaresapi.domain.objetosDeValor.CPF;
+import br.edu.infnet.pedrovalladaresapi.validation.annotations.ValidCpf;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CpfValidator implements ConstraintValidator<ValidCpf, CPF>{
+    @Override
+    public boolean isValid(CPF cpf, ConstraintValidatorContext constraintValidatorContext) {
+        if(cpf.getCpf().isEmpty())
+            return false;
+
+        return cpf.valido();
+    }
+}

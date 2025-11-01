6a240ac5d16c157875730b9fe66025b7a37cdf02
Pedro Valladares <pedrohvalladares@hotmail.com>
2025-11-01 14:29:45 -0300

    adicionado demais endpoints de transporte controller

--- src/main/java/br/edu/infnet/pedrovalladaresapi/controllers/TransporteController.java
+++ src/main/java/br/edu/infnet/pedrovalladaresapi/controllers/TransporteController.java
@@ -1,5 +1,6 @@
 package br.edu.infnet.pedrovalladaresapi.controllers;
 
+import br.edu.infnet.pedrovalladaresapi.domain.DTOs.transporte.AlterarTransporteDTO;
 import br.edu.infnet.pedrovalladaresapi.domain.models.Transporte;
 import br.edu.infnet.pedrovalladaresapi.requests.RequestResponse;
 import br.edu.infnet.pedrovalladaresapi.services.TransporteService;
@@ -27,5 +28,36 @@ public class TransporteController {
     public ResponseEntity<RequestResponse> obterPorCodigo(@PathVariable String codigo){
         Transporte transporte = transporteService.obterPorCodigo(codigo);
         return transporte == null ? RequestResponse.getByCode(HttpStatus.NOT_FOUND, null) : RequestResponse.getByCode(HttpStatus.OK, transporte);
+    }
+
+    @GetMapping
+    public ResponseEntity<RequestResponse> obterTodos(){
+        var transportes = transporteService.listarTodos();
+        return transportes.isEmpty() ? RequestResponse.getByCode(HttpStatus.NO_CONTENT, null) : RequestResponse.getByCode(HttpStatus.OK, transportes);
+    }
+
+    @PutMapping("/{codigo}")
+    public ResponseEntity<RequestResponse> alterar(@PathVariable String codigo, @RequestBody AlterarTransporteDTO alterarTransporteDTO){
+        Transporte transporteAlterado = new Transporte();
+        transporteAlterado.setCodigo(codigo);
+        transporteAlterado.setItinerario(alterarTransporteDTO.getItinerario());
+        transporteAlterado.setTarifa(alterarTransporteDTO.getTarifa());
+        transporteAlterado.setEstado(alterarTransporteDTO.getEstado());
+        transporteAlterado.setAtivo(alterarTransporteDTO.getAtivo());
+
+        transporteAlterado = transporteService.alterar(codigo, transporteAlterado);
+
+        return transporteAlterado == null ? RequestResponse.getByCode(HttpStatus.NOT_FOUND, transporteAlterado) : RequestResponse.getByCode(HttpStatus.OK, transporteAlterado);
+    }
+
+    @PatchMapping("/inativar/{codigo}")
+    public ResponseEntity<RequestResponse> inativar(@PathVariable String codigo){
+        Transporte transporte = transporteService.obterPorCodigo(codigo);
+
+        if(transporte == null)
+            return RequestResponse.getByCode(HttpStatus.NOT_FOUND, null);
+
+        transporte.setAtivo(false);
+        return RequestResponse.getByCode(HttpStatus.OK, transporteService.alterar(codigo, transporte));
     }
 }

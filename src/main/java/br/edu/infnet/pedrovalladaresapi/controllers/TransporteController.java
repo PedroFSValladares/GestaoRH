101a0bf8652c4ae417c5b82d24220ef81d0c5e18
Pedro Valladares <pedrohvalladares@hotmail.com>
2025-11-01 13:06:04 -0300

    ajuste no transporte controller

--- src/main/java/br/edu/infnet/pedrovalladaresapi/controllers/TransporteController.java
+++ src/main/java/br/edu/infnet/pedrovalladaresapi/controllers/TransporteController.java
@@ -1,8 +1,9 @@
 package br.edu.infnet.pedrovalladaresapi.controllers;
 
 import br.edu.infnet.pedrovalladaresapi.domain.models.Transporte;
-import br.edu.infnet.pedrovalladaresapi.requests.ResponseBody;
+import br.edu.infnet.pedrovalladaresapi.requests.RequestResponse;
 import br.edu.infnet.pedrovalladaresapi.services.TransporteService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,15 +18,14 @@ public class TransporteController {
     }
 
     @PostMapping
-    public ResponseEntity<ResponseBody> incluir(@RequestBody Transporte transporte){
-        ResponseBody responseBody = ResponseBody.Created(transporteService.incluir(transporte));
-        return ResponseEntity.status(responseBody.getStatus()).body(responseBody);
+    public ResponseEntity<RequestResponse> incluir(@RequestBody Transporte transporte){
+        Transporte transporteCriado = transporteService.incluir(transporte);
+        return RequestResponse.getByCode(HttpStatus.CREATED, transporteCriado);
     }
 
     @GetMapping("/{codigo}")
-    public ResponseEntity<ResponseBody> obterPorCodigo(@PathVariable String codigo){
+    public ResponseEntity<RequestResponse> obterPorCodigo(@PathVariable String codigo){
         Transporte transporte = transporteService.obterPorCodigo(codigo);
-        ResponseBody responseBody = transporte == null ? ResponseBody.NotFound() : ResponseBody.Ok(transporte);
-        return ResponseEntity.status(responseBody.getStatus()).body(responseBody);
+        return transporte == null ? RequestResponse.getByCode(HttpStatus.NOT_FOUND, null) : RequestResponse.getByCode(HttpStatus.OK, transporte);
     }
 }

c497f5cdbe8e6c5df1d080b7724afffc148e6396
Pedro Valladares <pedrohvalladares@hotmail.com>
2025-11-05 17:46:14 -0300

    adição do dockerfile

--- src/main/java/br/edu/infnet/pedrovalladaresapi/loaders/BaseLoader.java
+++ src/main/java/br/edu/infnet/pedrovalladaresapi/loaders/BaseLoader.java
@@ -2,22 +2,25 @@ package br.edu.infnet.pedrovalladaresapi.loaders;
 
 import org.springframework.boot.ApplicationRunner;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class BaseLoader implements ApplicationRunner {
     public List<String> obterLinhasDeArquivo(String nomeDoArquivo, Boolean ignorarCabecalho) throws IOException {
-        FileReader arquivo = new FileReader(nomeDoArquivo);
-        BufferedReader leitura = new BufferedReader(arquivo);
+        File file = new File(nomeDoArquivo);
+        if(file.exists()){
+            FileReader arquivo = new FileReader(file);
+            BufferedReader leitura = new BufferedReader(arquivo);
 
-        List<String> linhas = new ArrayList<>(leitura.lines().toList());
-        if (ignorarCabecalho)
-            linhas.remove(0);
-        arquivo.close();
-        return linhas;
+            List<String> linhas = new ArrayList<>(leitura.lines().toList());
+            if (ignorarCabecalho)
+                linhas.remove(0);
+            arquivo.close();
+            return linhas;
+        }else{
+            System.err.println("Erro ao tentar abrir o arquivo:" + file.getAbsolutePath());
+            return new ArrayList<>();
+        }
     }
 }
